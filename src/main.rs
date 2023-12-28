d0baf05fc1368577c66af14116332313fa02c6c1
José Augusto <josemoura212@gmail.com>
2023-12-28 14:01:54 -0300

    Refactor code and add new variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, Jose!");
+    // println!("Hello, Jose!");
+
+    // let texto = String::from("Hello, Jose!");
+
+    // let texto2 = texto;
+
+    // let mut valor = 50;
+    // let referencia = &mut valor;
+
+    std::println!("Hello, Jose!");
+
+    const NAME_COMPANY: &str = "MangaTrix";
+    let name_company = "MangaTrix";
+
+    println!("nome empresa : {}", NAME_COMPANY);
+    println!("nome da variavel : {}", name_company);
+
+    let mut name_pessoa = "Jose";
+    println!("nome pessoa : {}", name_pessoa);
+    name_pessoa = "treinamento";
+    println!("nome curso : {}", name_pessoa);
 }
